448e03dda0ded4ea24596f1dc65a5ad0ba4ef88d
gabrielms-castro <gabrielms.castro@gmail.com>
2025-11-02 13:42:44 -0300

    add some front-end

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,6 +1,13 @@
 import type { BunRequest } from "bun";
 import type { ApiConfig } from "../config";
+import { BadRequestError } from "./errors";
+import { getUserByEmail } from "../db/users";
 
 export async function handlerLogin(config: ApiConfig, req: BunRequest) {
-    return ""
+    const { email, password } = await req.json();
+    if (!email || !password) {
+        throw new BadRequestError("É necessário fornecer um e-mail e uma senha")
+    }
+    const user = await getUserByEmail(config.db, email)
+    if (!user) throw new 
 }
