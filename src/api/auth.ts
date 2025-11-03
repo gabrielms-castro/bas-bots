cf2cfd3deaa0d9df2af66ebca59afd402df3cc64
gabrielms-castro <gabrielms.castro@gmail.com>
2025-11-03 13:07:12 -0300

    add generate jwt when login

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,8 +2,9 @@ import type { BunRequest } from "bun";
 import type { ApiConfig } from "../config";
 import { BadRequestError, NotFoundError, UserNotAuthenticatedError } from "./errors";
 import { getUserByEmail } from "../db/users";
-import { checkPasswordHash } from "../auth";
+import { checkPasswordHash, makeJWT, makeRefreshToken } from "../auth";
 import { respondWithJSON } from "./json";
+import { createRefreshToken } from "../db/refresh-tokens";
 
 export async function handlerLogin(config: ApiConfig, req: BunRequest) {
     const { email, password } = await req.json();
@@ -20,7 +21,21 @@ export async function handlerLogin(config: ApiConfig, req: BunRequest) {
     if (!valid) {
         throw new UserNotAuthenticatedError("E-mail ou senha incorretos. Tente novamente")
     }
+
+    const accessExpiresMs = 30 * 24 * 60 * 60 * 1000;
+    const accessToken = makeJWT(user.id, config.jwtSecret, accessExpiresMs)
+
+    const refreshExpiresMs = 60 * 24 * 60 * 60 * 1000;
+    const refreshToken = makeRefreshToken();
+    await createRefreshToken(config.db, {
+        token: refreshToken,
+        userID: user.id,
+        expiresAt: new Date(Date.now() + refreshExpiresMs)
+    })
+
     return respondWithJSON(200,{
         user,
+        token: accessToken,
+        refreshToken: refreshToken
     })
 }
